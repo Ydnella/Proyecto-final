abbbde6b4550fabdce8bc8ff6dfc2e0841eda1d0
Ydnella <Ottobreu54@gmail.com>
2025-03-14 21:34:27 -0400

    // Content: actualizacion

--- Main.py
+++ Main.py
@@ -3,11 +3,20 @@ import Funciones_de_tareas
 
 
 def menu():
-    print("1. Crear tarea")
-    print("2. Leer tareas")
-    print("3. Actualizar tarea")
-    print("4. Cargar tareas")
-    print("5. Salir")
+    print("----------------------------------")
+    print("\n")
+    print("Bienvenido al sistema de tareas")
+    print("\n")
+    print("         Menú principal          ")
+    print("\n")
+    print("         1. Crear tarea          ")
+    print("         2. Leer tareas          ")
+    print("         3. Actualizar tarea     ")
+    print("         4. Cargar tareas        ")
+    print("\n")
+    print("         5. Salir")
+    print("\n")
+    print("----------------------------------")
     return int(input("Seleccione una opción: "))
 
 
@@ -17,22 +26,22 @@ if __name__ == "__main__":
         if opcion == 1:
             while True:
                 Funciones_de_tareas.crear_tarea()
-                if input("¿Desea volver al menú principal? (s/n): ").lower() == 's':
+                if input("Desea volver al menu principal? (s/n): ").lower() == 's':
                     break
         elif opcion == 2:
             while True:
                 Funciones_de_tareas.leer_tareas()
-                if input("¿Desea volver al menú principal? (s/n): ").lower() == 's':
+                if input("Desea volver al menu principal? (s/n): ").lower() == 's':
                     break
         elif opcion == 3:
             while True:
                 Funciones_de_tareas.cargar_tareas()
-                if input("¿Desea volver al menú principal? (s/n): ").lower() == 's':
+                if input("Desea volver al menu principal? (s/n): ").lower() == 's':
                     break
         elif opcion == 4:
             while True:
                 Funciones_de_tareas.actualizar_tarea()
-                if input("¿Desea volver al menú principal? (s/n): ").lower() == 's':
+                if input("Desea volver al menu principal? (s/n): ").lower() == 's':
                     break
         elif opcion == 5:
             break
